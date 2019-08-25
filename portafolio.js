a547c6b53f14dbcacb91e65a52811b4f8fd9617d
Paula <54006298+paulamellado@users.noreply.github.com>
2019-08-25 01:39:32 -0400

    Descripciones

--- portafolio.js
+++ portafolio.js
@@ -3,40 +3,40 @@ var trabajos = [
   {
       figure: "media/trabajo-01-bslide.jpg",
       alt: "Descripción de la imagen",
-      title: "Título de sexto trabajo",
-      about: "Texto simulado de descripción de sexto trabajo",
+      title: "B-Slide",
+      about: "Modelo de colmena de extracción horizontal y distribución vertical que optimiza el proceso productivo apícola. Diseñado para la clase de Taller Calidad I en conjunto con FIA.",
       by: "by: paula"
   },
 
   {
       figure: "media/trabajo-02-bioef.jpg",
       alt: "Descripción de la imagen",
-      title: "Título de séptimo trabajo",
-      about: "Texto simulado de descripción de séptimo trabajo",
+      title: "Bioef",
+      about: "Limpiador y/o desinfectante bioefervescente en formato de pastilla, amigable con el medioambiente. Fue diseñado para la clase de Taller Calidad I en relación al desafío Virutex-Ilko.",
       by: "by: paula"
   },
 
   {
       figure: "media/trabajo-03-cesfam.jpg",
       alt: "Descripción de la imagen",
-      title: "Título de séptimo trabajo",
-      about: "Texto simulado de descripción de séptimo trabajo",
+      title: "Rediseño de servicio CESFAM MTC",
+      about: "Examen para la clase de Introducción al Diseño de Servicios con Red Salud Áncora. Trabajo de diseño colaborativo para disminuir el estrés de las TENS de farmacia.",
       by: "by: paula"
   },
 
   {
       figure: "media/trabajo-04-golos.jpg",
       alt: "Descripción de la imagen",
-      title: "Título de séptimo trabajo",
-      about: "Texto simulado de descripción de séptimo trabajo",
+      title: "Golos",
+      about: "Dispositivo para sillas de ruedas que permite tener el control de esta con una sola mano. Ideal para personas que juegan bolos. Diseñado para la clase de Taller Producto con Ride de Vuelta.",
       by: "by: paula"
   },
 
 {
     figure: "media/trabajo-05-propio.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
-    about: "Texto simulado de descripción de quinto trabajo",
+    title: "Propio",
+    about: "Mobiliario divisor para albergues fabricado con 90% de tela PVC reciclada. El armazón está hecho con tubos de tela termosellada y endurecida. Diseñado para Taller Producto.",
     by: "by: cata y paula"
 },
 
@@ -44,7 +44,7 @@ var trabajos = [
   figure: "media/trabajo-06-comic.jpg",
   alt: "Descripción de la imagen",
   title: "Título de primer trabajo",
-  about: "Texto simulado de descripción de primer trabajo",
+  about: "La serie de cómics mensuales para el diario Visión trata sobre situaciones cotidianas de los estudiantes relacionadas a eventos que ocurren de acuerdo al mes de su respectiva publicación.",
   by: "by: cata"
 },
 
@@ -52,7 +52,7 @@ var trabajos = [
   figure: "media/trabajo-07-concurso.jpg",
   alt: "Descripción de la imagen",
   title: "Título de segundo trabajo",
-  about: "Texto simulado de descripción de segundo trabajo",
+  about: "Ilustración para concurso de “Talk Talk Korea 2018”. El tema era la paz en Corea, donde se usó como referencia eventos deportivos donde Corea del Norte y del Sur participaron juntos.",
   by: "by: cata"
 },
 
@@ -60,7 +60,7 @@ var trabajos = [
   figure: "media/trabajo-08-camile.jpg",
   alt: "Descripción de la imagen",
   title: "Título de tercer trabajo",
-  about: "Texto simulado de descripción de tercer trabajo",
+  about: "Ilustración de Camile, un personaje femenino de un cómic personal que se encuentra en proceso de redacción del guión para luego ilustrar las escenas de la historia.",
   by: "by: cata"
 },
 
@@ -68,15 +68,15 @@ var trabajos = [
   figure: "media/trabajo-09-day6.jpg",
   alt: "Descripción de la imagen",
   title: "Título de cuarto trabajo",
-  about: "Texto simulado de descripción de cuarto trabajo",
+  about: "Fondo de los miembros de la banda surcoreana “Day6” ilustrada para un mapa del juego online “osu!”. La canción mapeada fue “Man in a movie” de la misma banda por el jugador iYiyo.",
   by: "by: cata"
 },
 
 {
     figure: "media/trabajo-10-pandarojo.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo",
+    title: "Enriquecimiento ambiental Panda Rojo",
+    about: "Sistema de enriquecimiento ambiental conformado por puentes tejidos y una plataforma. Todos los materiales utilizados son reciclados. Diseñado para la clase de Taller Interacción.",
     by: "by: cata y paula"
 },
 
